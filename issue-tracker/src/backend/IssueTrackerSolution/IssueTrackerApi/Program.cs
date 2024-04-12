35d4b0982851e673bbc5a549fbfcf795eb4b5798
Jeff Gonzalez <Jeff@hypertheory.com>
2024-04-12 10:58:21 -0400

    fixed consumer group

--- issue-tracker/src/backend/IssueTrackerSolution/IssueTrackerApi/Program.cs
+++ issue-tracker/src/backend/IssueTrackerSolution/IssueTrackerApi/Program.cs
@@ -35,17 +35,16 @@ builder.Host.UseWolverine(opts =>
     {
         c.AutoOffsetReset = Confluent.Kafka.AutoOffsetReset.Earliest; // Earliest here means the earliest this consumer group hasn't already processed.
         // in Kafka, consumer groups are managed by the broker - it *knows*. 
+        c.GroupId = "issue-tracker-api";
 
     });
 
     opts.ListenToKafkaTopic("softwarecenter.catalog-item-created")
-        .ProcessInline()
-        .WithGroupId("issue-tracker-api"); // This is the consumer group.
-                                           // Wolverine, because it works with other brokers aside from kafka *also* keeps track of the offset.
+        .ProcessInline();
+
 
     opts.ListenToKafkaTopic("softwarecenter.catalog-item-retired")
-        .ProcessInline()
-        .WithGroupId("issue-tracker-api");
+        .ProcessInline();
 
     opts.PublishMessage<IssueCreated>().ToKafkaTopic("help-desk.issue-created");
     opts.Services.AddResourceSetupOnStartup(); // sort of like the entity framework stuff to create the tables wolverine needs
